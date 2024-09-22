7e4dd4bfa0c95fcddadd87bcb2f96672d77a67cc
Ojaswi Athghara <ojaswi@acalvio.com>
2024-09-22 16:08:09 +0530

    minor improvements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import ToggleTheme from "./ToggleTheme";
 
 function Navbar() {
     return (
-        <nav className="w-full h-16 bg-primary flex items-center px-4">
+        <nav className="w-full h-12 bg-primary flex items-center px-4">
             <button className="btn btn-outline bg-primary border-white text-white btn-sm">
                 Login
             </button>
