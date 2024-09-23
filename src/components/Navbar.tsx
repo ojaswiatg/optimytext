ac04576623d31203351dbbbdd385dd9d56b519ff
Ojaswi Athghara <ojaswi@acalvio.com>
2024-09-23 16:09:52 +0530

    file restructuring

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,8 @@ import ToggleTheme from "./ToggleTheme";
 function Navbar() {
     return (
         <nav className="w-full h-12 bg-primary flex items-center px-4">
+            {/* if logged in, show a burger menu with: history, logout */}
+
             <button className="btn btn-outline bg-primary border-white text-white btn-sm">
                 Log in
             </button>
